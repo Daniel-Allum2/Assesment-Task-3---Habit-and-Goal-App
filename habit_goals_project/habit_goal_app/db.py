c14f51fe76da4fa2a0b68177165bce2765a3d1ba
Daniel-Allum2 <danielallum@icloud.com>
2026-06-16 09:07:02 +0000

    def get_db

--- habit_goals_project/habit_goal_app/db.py
+++ habit_goals_project/habit_goal_app/db.py
@@ -15,3 +15,14 @@ sqlite3.register_converter(
     "date",
     lambda value: date.fromisoformat(value.decode("utf-8")),
 )
+
+
+def get_db():
+    if "db" not in g:
+        g.db = sqlite3.connect(
+            current_app.config["DATABASE"],
+            detect_types=(sqlite3.PARSE_DECLTYPES | sqlite3.PARSE_COLNAMES),
+        )
+    g.db.row_factory = sqlite3.Row
+    g.db.execute("PRAGMA foreign_keys = ON")
+    return g.db
