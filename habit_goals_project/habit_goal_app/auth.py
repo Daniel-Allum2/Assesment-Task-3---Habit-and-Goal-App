ec428546d582fa4e965d02442880433c47a8d276
Daniel-Allum2 <danielallum@icloud.com>
2026-06-20 02:22:35 +0000

    Imports in auth.py file copied from other template

---
+++ habit_goals_project/habit_goal_app/auth.py
@@ -0,0 +1,19 @@
+import functools
+import sqlite3
+
+from flask import (
+    Blueprint,
+    flash,
+    g,
+    redirect,
+    render_template,
+    request,
+    session,
+    url_for,
+)
+
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from .db import get_db
+
+bp = Blueprint("auth", __name__, url_prefix="/auth")
